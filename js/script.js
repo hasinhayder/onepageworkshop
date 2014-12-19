dd21a4fe215d5ac10434a573b96a7eec405cf315
Hasin Hayder <hasin@leevio.com>
2014-12-19 15:05:02 +0600

    Foundation with Banner

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by storyteller on 12/19/14.
+ */
+;(function($){
+    $(document).ready(function(){
+        $(".banner").backstretch([
+            "img/b1.jpg",
+            "img/b16.jpg",
+            "img/b20.jpg"
+        ], {duration: 3000, fade: 750});
+    });
+})(jQuery);
